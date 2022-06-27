4d9b35ec3ea42d9d3f1c427a2bb5af1337a650fe
Andrey Vlasov <andreyperm109@mail.ru>
2022-06-27 23:49:28 +0500

    web-app is DONE! (but still have some small bugs)

---
+++ php/test_mail.php
@@ -0,0 +1,6 @@
+<?php    
+    $headers ='Content-type:text/html; charset = windows-1251 \r\n';
+    $headers .='From: oursystem@gmail.com';
+    $headers .='Reply to oursystem@gmail.com';
+    mail("andreyperm109@mail.ru","TEST Change","Hello",$headers);
+?>
